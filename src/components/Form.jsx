1a8c7207771edb85e53b3dd9bf0752c120a0e74f
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2024-01-20 23:43:42 +0700

    set itemName & quantity with useState

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react"
+
 const Form = () => {
+    const [name, setName] = useState('')
+    const [quantity, setQuantity] = useState(1)
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     const quantityNum = [...Array(20)].map((_, index) => (
         <option value={ index + 1 } key={ index + 1 }>{ index  + 1 }</option>
     ))
 
     return(
-        <form className="add-form">
+        <form className="add-form" onSubmit={ handleSubmit }>
             <h3>Hari ini belanja apa kita?</h3>
             <div>
                 <select>{ quantityNum }</select>
-            <input type="text" placeholder="nama barang..." />
+                <input type="text" placeholder="Item name ..." 
+                    value={ name } 
+                    onChange={(event) => setName(event.target.value) } 
+                />
             </div>
         <button>Tambah</button>
     </form>
