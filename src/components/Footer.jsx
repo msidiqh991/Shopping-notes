10ffeb9e2942e6a72378d615d825d232d0d66408
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2024-01-21 16:57:34 +0700

    add guard validate when notes empty

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,11 @@ const Footer = ({ items }) => {
     const checkedItems = items.filter((item) => item.checked).length
     const percentage = Math.round((checkedItems / totalItems) * 100)
 
+    if(items.length === 0)
+        return <footer className="stats">The shopping list is still empty</footer>
+
     return <footer className="stats">
-        Ada { totalItems } barang di daftar belanjaan, { checkedItems } barang sudah dibeli ({ percentage }%)
+        There are { totalItems } items on the shopping list, { checkedItems } items have been purchased ({ percentage }%)
         </footer>
 }
 
