14eaf02ad663e72f695115bdb39da621b94a59d0
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2024-01-21 17:25:34 +0700

    add variant dummy data

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -17,6 +17,48 @@ const GroceryItem = [
     quantity: 3,
     checked: false,
   },
-]
+  {
+    id: 4,
+    name: 'Bread',
+    quantity: 2,
+    checked: false,
+  },
+  {
+    id: 5,
+    name: 'Milk',
+    quantity: 1,
+    checked: true,
+  },
+  {
+    id: 6,
+    name: 'Eggs',
+    quantity: 12,
+    checked: false,
+  },
+  {
+    id: 7,
+    name: 'Cheese',
+    quantity: 3,
+    checked: true,
+  },
+  {
+    id: 8,
+    name: 'Tomatoes',
+    quantity: 4,
+    checked: false,
+  },
+  {
+    id: 9,
+    name: 'Chicken Breast',
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 10,
+    name: 'Potatoes',
+    quantity: 5,
+    checked: false,
+  },
+];
 
 export default GroceryItem
